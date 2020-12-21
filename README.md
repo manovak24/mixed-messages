1e3f22698b81ffc107c6fa4e98da37d827586971
emagdnimMQT <mark.w.novak@gmail.com>
2020-12-21 10:35:56 -0500

    Project complete for now. Might come back to add more features

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Project Name
-> Mixed Messages - A random message generator to help Instagram influencers come up with post descriptions based on the category of post types: Outdoors, Motivational, Dad Jokes
+> Mixed Messages - A simple Node.js console app to generate random quotes for Instagram influencers to help with post descriptions based on the category of post types: Outdoors, Motivational and Fitness.
 
 ## Table of contents
 * [General info](#general-info)
-* [Screenshots](#screenshots)
 * [Technologies](#technologies)
 * [Setup](#setup)
 * [Features](#features)
@@ -12,38 +11,51 @@
 * [Contact](#contact)
 
 ## General info
-Add more general information about project. What the purpose of the project is? Motivation?
+This was a JavaScript Syntax portfolio project for the Codecademy Full-Stack Engineer course. The idea was to build a Node.js console app that generates random messages each time a user runs the program and version your project with Git/GitHub.
 
-## Screenshots
-![Example screenshot](./img/screenshot.png)
+I decided to come up with a simple console app that generates a random quote from arrays stored within an object that could be used by an Instagram influencer to come up with quotes based on desired categories. This consnole app uses a for...in loop and a switch statement to iterate throught the object's keys and return a random quote from each category.
 
 ## Technologies
-* Tech 1 - version 1.0
-* Tech 2 - version 2.0
-* Tech 3 - version 3.0
+* JavaScript - version ES6
+* node - version 12.18.0
 
 ## Setup
-Describe how to install / setup your local environement / add link to demo version.
+Clone respository from https://github.com/manovak24/mixed-messages.git
+
+To start your server, run `npm install` and then `node main.js`.
 
 ## Code Examples
-Show examples of usage:
-`put-your-code-here`
+```js
+for (prop in quotes) {
+    let optionIdx = randomNumber(quotes[prop].length)
+    switch(prop) {
+        case 'outdoors':
+            quotesToPost.push(`Outdoor Quote - ${quotes[prop][optionIdx]}`)
+            break
+        case 'motivational':
+            quotesToPost.push(`Motivational Quote - ${quotes[prop][optionIdx]}`)
+            break
+        case 'fitness':
+            quotesToPost.push(`Fitness Quote - ${quotes[prop][optionIdx]}`)
+            break
+        default:
+            quotesToPost.push(`Nothing to post today`)
+    }
+}
+```
 
 ## Features
 List of features ready and TODOs for future development
-* Awesome feature 1
-* Awesome feature 2
-* Awesome feature 3
+* Generates a random quote from arrays stored within an object
+* Uses for...in loop to iterate over the keys in the object
+* Switch statement to generate the random quote from each array within the object
 
 To-do list:
-* Wow improvement to be done 1
-* Wow improvement to be done 2
+* Create site where this app could be used.
 
 ## Status
-Project is: _in progress_, _finished_, _no longer continue_ and why?
-
-## Inspiration
-Add here credits. Project inspired by..., based on...
+Project is: _finished_, and _in progress_
+>I would like to come back to make a site that the app could be used on
 
 ## Contact
-Created by [@flynerdpl](https://www.flynerd.pl/) - feel free to contact me!
+Created by [@manovak24](https://github.com/manovak24) - feel free to contact me!
--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 //Object containing three arrays with outdoor, motivational and fitness quotes
 const quotes = {
     outdoors: ['Waking up early on a weekend to hit the trail is a sacrifice made well worth it when you reach a mountain top in time to catch the sunrise.', 'When you are hiking through a forest, you have the chance to see the natural world up close.', 'Modern life has made the draw of home stronger than ever, but taking the time to unplug and hit the trail is the perfect way to reset and reassess your priorities.', 'The good tidings of the mountains are a real thing, just ask any thru-hiker, backpacker, or climber.', 'We agree with Muir, we get a lot more than a great workout out of every hike we take. '],
-    motivational: ['The harder I work, the luckier I get', 'The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.', 'Don’t Let Yesterday Take Up Too Much Of Today.', 'You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.'],
+    motivational: ['The harder I work, the luckier I get.', 'The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.', 'Don’t Let Yesterday Take Up Too Much Of Today.', 'You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.'],
     fitness: ['Today I will do what others won’t, so tomorrow I can accomplish what others can’t.', 'Do something today that your future self will thank you for.', 'We are what we repeatedly do. Excellence then is not an act but a habit.', 'No matter how slow you go, you are still lapping everybody on the couch.', 'Sweat is fat crying.']
 }
 
-//Returns random quote from array
-const randomQuote = (quote) => {
-   let randomIndex = Math.floor(Math.random() * 5);
-   quote = quote[randomIndex];
-   return{
-       quote
-   };
+//Returns random number to select random quote
+const randomNumber = (num) => {
+    return Math.floor(Math.random() * num);
 }
 
 //Store quotes in an array
-let quotesToPost = [];
+let today = new Date();
+let date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+let quotesToPost = [`Today is ${date} and your potential quotes to use with your Instagram posts are as follows:`];
 
 //Function to output the random quote generated
-const quoteGenerator = (quote) => {
-    let outdoorQuote = randomQuote(quote.outdoors);
-    let motivationalQuote = randomQuote(quote.motivational);
-    let fitnessQuote = randomQuote(quote.fitness);
-    return {
-        outdoorQuote, motivationalQuote, fitnessQuote
-    }  
+for (prop in quotes) {
+    let optionIdx = randomNumber(quotes[prop].length)
+    switch(prop) {
+        case 'outdoors':
+            quotesToPost.push(`Outdoor Quote - ${quotes[prop][optionIdx]}`)
+            break
+        case 'motivational':
+            quotesToPost.push(`Motivational Quote - ${quotes[prop][optionIdx]}`)
+            break
+        case 'fitness':
+            quotesToPost.push(`Fitness Quote - ${quotes[prop][optionIdx]}`)
+            break
+        default:
+            quotesToPost.push(`Nothing to post today`)
+    }
 }
 
-//Test to make sure randomQuote works
-/*console.log(randomQuote(quotes.outdoors));
-console.log(randomQuote(quotes.motivational));
-console.log(randomQuote(quotes.fitness));*/
+const formatQuote = () => {
+    const formatted = quotesToPost.join('\n ')
+    console.log(formatted)
+}
 
-//Test to see if quoteGenerator works
-console.log(quoteGenerator(quotes));
+formatQuote(quotesToPost);
